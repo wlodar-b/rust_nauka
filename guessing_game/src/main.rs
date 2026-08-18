3bc6fee064b435f86c79916e858693d428bcf1c3
Bartek <wlodarcykb16@gmail.com>
2026-08-18 13:24:55 +0200

    Refactor guessing_game logic to handle invalid input and improve game flow

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    loop {
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -15,16 +16,21 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
 
 
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            println!("The secret number was: {}", secret_number);
+            break;
+            }
+        }
     }
-
-    println!("The secret number is: {}", secret_number);
-
 }
