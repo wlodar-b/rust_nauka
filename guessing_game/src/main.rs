98976b0428f4a3c3c9d5870e07db9c9df8496130
Bartek <wlodarcykb16@gmail.com>
2026-08-18 13:07:35 +0200

    Update guessing_game with new dependencies and game logic enhancements

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
@@ -11,5 +15,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!("The secret number is: {}", secret_number);
+
 }
