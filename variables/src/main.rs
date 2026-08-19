a6cffd6f078c8aef6796204698d039e724042fbe
Bartek <wlodarcykb16@gmail.com>
2026-08-19 09:11:17 +0200

    Refactor main function to improve variable handling and output formatting

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let  x = 5;
+    let x = x + 1;
+
+    println!("Value of x is: {x}");
+
+    {
+        let x = x * 2;
+        println!("New value of x is: {x}");
+    }
+
+    println!("Value of x is again: {x}");
 }
