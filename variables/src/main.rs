f8da0a0a138042b50927a11df3b290848ca7aa85
Bartek <wlodarcykb16@gmail.com>
2026-08-19 12:13:46 +0200

    Refactor main function to remove unused variable and streamline input handling

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,13 +1,22 @@
+use std::io;
+
 fn main() {
-    let  x = 5;
-    let x = x + 1;
+    let a = [1, 2, 3, 4, 5];
 
-    println!("Value of x is: {x}");
+    println!("Please enter an array index.");
 
-    {
-        let x = x * 2;
-        println!("New value of x is: {x}");
-    }
+    let mut index = String::new();
 
-    println!("Value of x is again: {x}");
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
