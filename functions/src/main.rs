61dc414c78018e405ff1b436ba8a0908187b86db
Bartek <wlodarcykb16@gmail.com>
2026-08-19 12:59:49 +0200

    Refactor main function to implement plus_one and remove unused functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,15 +1,9 @@
 fn main() {
-    println!("Hello, world!");
-    print_labeled_measurement(5, 'h');
+    let x = plus_one(5);
 
-
-    another_function();
+    println!("The value of x is: {x}");
 }
 
-fn another_function() {
-    println!("Another function.");
-}
-
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
