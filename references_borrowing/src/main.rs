f51c76b49f6a15226133bdebce16e56853e7c740
Bartek <wlodarcykb16@gmail.com>
2026-08-24 14:01:30 +0200

    Refactor main function to return values from greet, preventing ownership issues

--- references_borrowing/src/main.rs
+++ references_borrowing/src/main.rs
@@ -1,3 +1,22 @@
+ /* fn main() {
+    let m1 = String::from("Hello");
+    let m2 = String::from("world");
+    greet(m1, m2);
+    let s = format!("{} {}", m1, m2); // Error: m1 and m2 are moved
+}
+
+fn greet(g1: String, g2: String) {
+    println!("{} {}!", g1, g2);
+} */
+
 fn main() {
-    println!("Hello, world!");
+    let m1 = String::from("Hello");
+    let m2 = String::from("world");
+    let (m1_again, m2_again) = greet(m1, m2);
+    let s = format!("{} {}", m1_again, m2_again);
+}
+
+fn greet(g1: String, g2: String) -> (String, String) {
+    println!("{} {}!", g1, g2);
+    (g1, g2)
 }
