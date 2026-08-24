fb87e8740b4951ca1561df77b236c50a120e6f2e
Bartek <wlodarcykb16@gmail.com>
2026-08-24 15:04:25 +0200

    Refactor main function to use references for string parameters, improving ownership handling

--- ownership/cwiczenia/src/main.rs
+++ ownership/cwiczenia/src/main.rs
@@ -1,15 +1,34 @@
-fn main() {
+/* fn main() {
     let towar = String::from("Laptop Poleasingowy Lenovo");
     
-    let klon_towaru = towar.clone();
-    wydrukuj_etykiete(towar);
-    zapisz_do_bazy(klon_towaru); // ❌ Tu kompilator rzuca błędem!
+    // Zamiast oddawać, WYPOŻYCZAMY (&) wskaźnik do towaru
+    wydrukuj_etykiete(&towar); 
+    
+    // Zmienna 'towar' wciąż jest nasza, więc możemy wypożyczyć ją ponownie!
+    zapisz_do_bazy(&towar); 
 }
 
-fn wydrukuj_etykiete(nazwa: String) {
+// Funkcja musi wiedzieć, że dostaje tylko referencję (&String), a nie wartość na własność
+fn wydrukuj_etykiete(nazwa: &String) {
     println!("Drukuję etykietę dla: {}", nazwa);
 }
 
-fn zapisz_do_bazy(nazwa: String) {
+fn zapisz_do_bazy(nazwa: &String) {
     println!("Zapisano w bazie: {}", nazwa);
+}
+ */
+
+fn main() {
+    let mut korytarz_a = vec!["Paleta 1", "Paleta 2"];
+    
+    // System zapisuje referencję do pierwszej palety
+    let pierwsza_paleta = &korytarz_a[0]; 
+    
+
+    // System drukuje raport
+    println!("Na froncie korytarza wciąż stoi: {}", pierwsza_paleta); 
+
+    // Wjeżdża nowa dostawa na koniec korytarza
+    korytarz_a.push("Paleta 3"); // ❌ Tu kompilator rzuca błędem!
+    
 }
