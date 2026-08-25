b61e7daef383d26bf011207d1c077b59743bee6c
Bartek <wlodarcykb16@gmail.com>
2026-08-25 09:02:25 +0200

    Refactor main function to demonstrate ownership and borrowing with updated comments

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,5 @@
 
+/* 
 fn main() {
     let first = String::from("Ferris");
     let full = add_suffix(first);
@@ -32,4 +33,22 @@ fn main() {
 fn add_suffix(mut name: String) -> String {
     name.push_str(" Jr.");
     name
+}
+    */
+
+fn main() {
+    // 1. Inicjalizujemy status palety. 
+    // Dzięki 'mut' ta zmienna dostaje pełny pakiet uprawnień: +R (odczyt), +W (zapis), +O (własność).
+    let mut status_palety = String::from("W PRZYJMOWANIU"); 
+        
+    // 3. W międzyczasie magazynier klika na skanerze, że paleta jest gotowa.
+    // Próbujemy dopisać tekst do oryginalnej zmiennej:
+    status_palety.push_str(" - ZAKOŃCZONE"); // ❌ TUTAJ KOMPILATOR UDERZA W STÓŁ!
+
+    // 2. Moduł raportujący WMS-a chce tylko podejrzeć status, więc go WYPOŻYCZAMY.
+    let raport = &status_palety; 
+
+
+    // 4. Moduł raportujący drukuje swój podgląd.
+    println!("Dzienny raport: {}", raport);
 }
