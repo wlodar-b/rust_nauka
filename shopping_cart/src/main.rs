d79f0b1855116cdecb329480875b6f0a92661867
Bartek <wlodarcykb16@gmail.com>
2026-09-01 11:58:05 +0200

    Implement shopping cart functionality with item listing and total calculation

--- shopping_cart/src/main.rs
+++ shopping_cart/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let mut koszyk: Vec<(String, u32, u32)> = vec![
+        (String::from("Chleb"), 5, 2),
+        (String::from("Mleko"), 4, 3),
+        (String::from("Baton"), 1, 100)
+    ];
+
+    wypisz_paragon(&koszyk);
+    let do_zaplaty = oblicz_sume(&koszyk);
+    println!("Całkowita kwota do zapłaty wynosi: {}zł", do_zaplaty);
+}
+
+fn wypisz_paragon(lista: &Vec<(String, u32, u32)>) {
+    for (nazwa, cena, ilosc) in lista {
+        println!("{} - {}szt. x {}zł", nazwa, ilosc, cena);
+    }
+}
+
+fn oblicz_sume(produkty: &Vec<(String, u32, u32)>) -> u32 {
+    let mut kwota = 0;
+
+    for (_nazwa, cena, ilosc) in produkty {
+    kwota += cena * ilosc;
+    }
+    kwota
 }
