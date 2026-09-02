342a5cda2d4b313f1da6b0a7afb1f44cd0d9a117
Bartek <wlodarcykb16@gmail.com>
2026-09-02 14:11:10 +0200

    Refactor main function and add logic to extract the middle of an array

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     s.push_str(" world"); // OK dopiero teraz, bo hello już nie jest używane
 }
 */
-
+/*
 fn reszta_zdania(tekst: &str) -> &str {
     let bajty = tekst.as_bytes(); // 1. Zamień tekst na bajty
 
@@ -24,4 +24,28 @@ fn main() {
     let reszta = reszta_zdania(&powitanie);
     
     println!("Tekst bez pierwszego słowa: {}", reszta); // Powinno wypisać: "rdzawi programiści"
+}
+*/
+
+fn srodek_tablicy(liczby: &[i32]) -> &[i32] {
+    let dlugosc = liczby.len();
+    
+    if dlugosc < 3 {
+        return &[]; // Zwracamy pusty wycinek, jeśli długość jest mniejsza niż 3
+    }
+    &liczby[1..(dlugosc - 1)]
+
+    // if ... { return &[]; }
+    // zwróć wycinek od 1 do dlugosc - 1
+}
+
+fn main() {
+    let wektor_punktow = vec![10, 20, 30, 40, 50];
+    let srodek_w = srodek_tablicy(&wektor_punktow);
+    // Używamy {:?} do wypisywania całych wycinków/tablic
+    println!("Środek wektora: {:?}", srodek_w); // Powinno wypisać: [20, 30, 40]
+
+    let krotka_tablica = [1, 2];
+    let srodek_t = srodek_tablicy(&krotka_tablica);
+    println!("Środek tablicy: {:?}", srodek_t); // Powinno wypisać: []
 }
