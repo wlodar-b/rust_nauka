b9a55a2eae79858c577cfa8e6f133138579c5754
Bartek <wlodarcykb16@gmail.com>
2026-09-02 10:26:08 +0200

    Refactor main function and implement string slicing logic to return text after the first word

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,11 +1,27 @@
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes(); // konwersja String -> tablica bajtów
+/*
+fn main() {
+    let mut s = String::from("hello");
+    let hello: &str = &s[0..5]; // s traci W i O na czas życia hello
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' { // b' ' - literał bajtu reprezentujący spację
-            return i; // znaleziono spację - zwróć jej indeks
+    println!("{hello}");
+
+    s.push_str(" world"); // OK dopiero teraz, bo hello już nie jest używane
+}
+*/
+
+fn reszta_zdania(tekst: &str) -> &str {
+    let bajty = tekst.as_bytes(); // 1. Zamień tekst na bajty
+
+    for (i, &item) in bajty.iter().enumerate() {
+        if item == b' ' { // 2. Przeiteruj po nich szukając spacji
+            return &tekst[(i + 1)..]; // 3. Zwróć wycinek od (i + 1) do końca
         }
-    }
+    }  ""// 4. Zwróć "" jeśli nie ma spacji
+}
 
-    s.len() // brak spacji - całe słowo to cały string
+fn main() {
+    let powitanie = String::from("Witajcie rdzawi programiści");
+    let reszta = reszta_zdania(&powitanie);
+    
+    println!("Tekst bez pierwszego słowa: {}", reszta); // Powinno wypisać: "rdzawi programiści"
 }
