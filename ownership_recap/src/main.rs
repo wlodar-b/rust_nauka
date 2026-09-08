a94436fee1dbb27fd2622f3b800c2b6edf81a625
Bartek <wlodarcykb16@gmail.com>
2026-09-08 11:44:16 +0200

    Fix main function and remove commented-out code

--- ownership_recap/src/main.rs
+++ ownership_recap/src/main.rs
@@ -3,11 +3,21 @@ fn main() {
     let n = 0; 
     n += 1; // Błąd!
 }
-*/
 
 // NAPRAWIONY KOD 1:
 fn main() {
     let mut n = 0; // Teraz `n` ma uprawnienia: +R, +W, +O
     n += 1;        // Działa bez problemu!
     println!("{}", n);
+}
+*/
+
+fn main() {
+    let s = String::from("Hello world");
+    consume_a_string(s); 
+    println!("{s}"); // Błąd!
+}
+
+fn consume_a_string(_s: String) {
+    // om nom nom
 }
