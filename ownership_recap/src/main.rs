842270470700f56fc7e2fed61bb58e6b715617e5
Bartek <wlodarcykb16@gmail.com>
2026-09-08 14:42:48 +0200

    Refactor main function to demonstrate mutable reference usage and correct string manipulation

--- ownership_recap/src/main.rs
+++ ownership_recap/src/main.rs
@@ -55,10 +55,19 @@ fn consume_a_string(_s: String) {
 }
 */
 
+/*
 fn main() {
     let mut s = String::from("Hello");
     let s_ref = &mut s; // ZMIANA: Tworzymy referencję mutowalną (&mut)
     
     s_ref.push_str(" world"); // Działa! *s_ref ma teraz uprawnienia +R i +W.
     println!("{s_ref}");
+}
+*/
+
+fn main() {
+let mut s = String::from("Hello");
+let s_ref = &s;
+println!("{s_ref}");
+s.push_str(" world");
 }
