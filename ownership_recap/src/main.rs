1aad7f78e11f37df516ccd5be23801063ad6e9f1
Bartek <wlodarcykb16@gmail.com>
2026-09-08 11:42:47 +0200

    Fix variable mutability issue in main function and add output

--- ownership_recap/src/main.rs
+++ ownership_recap/src/main.rs
@@ -1,3 +1,13 @@
+/* 
 fn main() {
-    println!("Hello, world!");
+    let n = 0; 
+    n += 1; // Błąd!
+}
+*/
+
+// NAPRAWIONY KOD 1:
+fn main() {
+    let mut n = 0; // Teraz `n` ma uprawnienia: +R, +W, +O
+    n += 1;        // Działa bez problemu!
+    println!("{}", n);
 }
