825f4c8850394b4a169ee54e62907442931fa970
Bartek <wlodarcykb16@gmail.com>
2026-09-09 13:46:16 +0200

    Refactor main function to use build_product for creating Product instances

--- structy/src/main.rs
+++ structy/src/main.rs
@@ -4,12 +4,19 @@ struct Product {
     in_stock: bool,
 }
 
-fn main() {
-    let klawiatura = Product {
-        name: String::from("Klawiatura"),
-        price: 99.99,
+
+fn build_product(name: String, price: f64) -> Product {
+    Product {
+        name,
+        price,
         in_stock: true,
-    };
+    }
+}
+
+fn main() {
+    let mut klawiatura = build_product(String::from("Klawiatura mechaniczna"), 299.99);
 
 println!("Dostępny produkt to: {} w cenie: {} zł", klawiatura.name, klawiatura.price);
+klawiatura.in_stock = false;
+println!("Czy produkt jest dostępny? {}", klawiatura.in_stock);
 }
