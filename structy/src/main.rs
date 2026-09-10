c7641df0ff7bb9dd3fc5350a58ead98cd0992e2f
Bartek <wlodarcykb16@gmail.com>
2026-09-10 10:14:47 +0200

    Refactor main function and Product struct; remove commented-out code and add new product instantiation

--- structy/src/main.rs
+++ structy/src/main.rs
@@ -1,3 +1,4 @@
+/* 
 struct Product {
     name: String,
     price: f64,
@@ -20,3 +21,28 @@ println!("Dostępny produkt to: {} w cenie: {} zł", klawiatura.name, klawiatura
 klawiatura.in_stock = false;
 println!("Czy produkt jest dostępny? {}", klawiatura.in_stock);
 } 
+*/
+
+struct Product {
+    name: String,
+    price: f64,
+    in_stock: bool,
+}
+
+fn main() {
+    let klawiatura_v1 = Product {
+        name: String::from("Klawiatura mechaniczna"),
+        price: 350.00,
+        in_stock: true,
+    };
+
+    println!("Oryginalna nazwa: {}", klawiatura_v1.name);
+
+    // Tworzymy nową, tańszą wersję używając skrótu:
+    let klawiatura_v2 = Product {
+        price: 299.99,
+        ..klawiatura_v1
+    };
+
+    println!("Oryginalna cena: {}", klawiatura_v1.price);
+}
