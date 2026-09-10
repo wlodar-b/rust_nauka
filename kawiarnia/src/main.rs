15ae58c31d0febf98f8431f98f8dcef7cd67e34c
Bartek <wlodarcykb16@gmail.com>
2026-09-10 15:38:45 +0200

    Refactor main function to include multiple order serving and queue display calls

--- kawiarnia/src/main.rs
+++ kawiarnia/src/main.rs
@@ -17,6 +17,11 @@ let mut moja_kawiarnia = Cafe {
 
     add_order(&mut moja_kawiarnia, "Ania".to_string(), "Latte".to_string());
     add_order(&mut moja_kawiarnia, "Basia".to_string(), "Kawa".to_string());
+    show_queue(&moja_kawiarnia);
+    serve_next_order(&mut moja_kawiarnia);
+    show_queue(&moja_kawiarnia);
+    serve_next_order(&mut moja_kawiarnia);
+    show_queue(&moja_kawiarnia);
 }
 
 fn add_order(cafe: &mut Cafe, customer: String, drink: String) {
@@ -32,4 +37,24 @@ fn add_order(cafe: &mut Cafe, customer: String, drink: String) {
     cafe.queue.push(nowe_zamowienie);
 
     cafe.next_id += 1;
+}
+
+fn serve_next_order(cafe: &mut Cafe) {
+    if cafe.queue.is_empty() {
+        println!("Brak zamówień do obsłużenia.");
+    } else {
+        let gotowe_zamowienie = cafe.queue.remove(0);
+        
+        println!("Wydano: {} dla {} (Zamówienie #{})", gotowe_zamowienie.drink, gotowe_zamowienie.customer, gotowe_zamowienie.id);
+    }
+}
+
+fn show_queue(cafe: &Cafe) {
+    if cafe.queue.is_empty() {
+        println!("Kolejka jest pusta.");
+    } else {
+        for order in &cafe.queue {
+            println!("Zamówienie: {} dla: {} (Zamówienie #{})", order.drink, order.customer, order.id);
+        }
+    }
 }
