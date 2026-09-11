a61c73eb6a5fe7b197b64759f17c3b34990930c9
Bartek <wlodarcykb16@gmail.com>
2026-09-11 15:10:10 +0200

    Refactor Wallet implementation to improve method organization and add transfer functionality

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -3,24 +3,18 @@ struct Wallet {
     balance: u32,
 }
 
-// Tutaj otwieramy blok implementacji dla naszego portfela
 impl Wallet {
     // METODA 1: Tylko odczyt (niemutowalne pożyczenie)
-    // Zadanie: Wypisz w konsoli tekst, np.: "Portfel gracza: [owner], stan konta: [balance] tokenów."
     fn show_info(&self) {
         println!("Portfel gracza: {} ,stan konta: {} tokenów", self.owner, self.balance);
     }
 
     // METODA 2: Wpłata (mutowalne pożyczenie)
-    // Zadanie: Dodaj wartość 'amount' do obecnego balansu portfela.
     fn receive_funds(&mut self, amount: u32) {
         self.balance += amount
     }
 
     // METODA 3: Wypłata (mutowalne pożyczenie + logika)
-    // Zadanie: Sprawdź, czy na koncie jest wystarczająco środków (balance >= amount).
-    // Jeśli tak: odejmij 'amount' od balansu i wypisz "Wysłano [amount] tokenów.".
-    // Jeśli nie: wypisz "Błąd: Niewystarczająca ilość środków!".
     fn send_funds(&mut self, amount: u32) {
             if self.balance >= amount {
                 self.balance -= amount;
@@ -29,23 +23,38 @@ impl Wallet {
                 println!("Niewystarczają ilość środków");
             }
     }
-}
 
+
+// METODA 4: Przelew do innego portfela
+    fn transfer(&mut self, receiver: &mut Wallet, amount: u32) {
+        if self.balance >= amount {
+            self.balance -= amount;
+            receiver.balance += amount;
+            println!("Przelano {} tokenów od {} do {}", amount, self.owner, receiver.owner);
+        }   else {
+            println!("Błąd: Niewystarczająca ilość środków na przelew!");
+        }
+    }
+}
 fn main() {
-    // 1. Tworzymy nową, mutowalną instancję portfela
     let mut my_wallet = Wallet {
         owner: "Bartek".to_string(),
-        balance: 100, // Zaczynamy z 100 tokenami
+        balance: 100,
     };
 
-    // 2. Testujemy metody (zobacz, jak pięknie i obiektowo to teraz wygląda!)
+    let mut alice_wallet = Wallet {
+        owner: "Alice".to_string(),
+        balance: 50,
+    };
+
+    println!("Stan początkowy portfeli:");
     my_wallet.show_info();
+    alice_wallet.show_info();
     
-    my_wallet.receive_funds(50);
-    my_wallet.show_info(); // Powinno być 150
-    
-    my_wallet.send_funds(200); // Powinno wywalić błąd braku środków
-    
-    my_wallet.send_funds(30);
-    my_wallet.show_info(); // Powinno zostać 120
+    println!("Próba przelewu środków na konto alice");
+    my_wallet.transfer(&mut alice_wallet, 30);
+
+    println!("Stan po przelewie:");
+    my_wallet.show_info();
+    alice_wallet.show_info();
 }
